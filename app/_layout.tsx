65b9f74cf7126367ddb0cba48f2d3ee3e673f12c
Baris Korkmaz <barskorkmaz6@gmail.com>
2025-03-26 13:53:45 +0300

    uygulama arayüzü

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+import { AuthProvider } from '@/contexts/authContext'
+
+const StackLayout = () => {
+    return <Stack screenOptions={{ headerShown: false }}></Stack>
+}
+
+export default function RootLayout() {
+    return (
+        <AuthProvider>
+            <StackLayout />
+        </AuthProvider>
+    )
+}
+
+const styles = StyleSheet.create({})
