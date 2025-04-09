4e66bf094ef77fa011f9b44abab8152691fc3f7b
Baris Korkmaz <barskorkmaz6@gmail.com>
2025-04-09 17:02:57 +0300

    model görünümü düzenlendi

---
+++ components/ModalWrapper.tsx
@@ -0,0 +1,28 @@
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { colors, spacingY } from '@/constants/theme'
+import { ModalWrapperProps } from '@/types'
+
+const isIos = Platform.OS === 'ios';
+
+const ModalWrapper = ({
+    style,
+    children,
+    bg = colors.neutral800,
+}: ModalWrapperProps) => {
+    return (
+        <View style={[styles.container, { backgroundColor: bg }, style && style]}>
+            {children}
+        </View>
+    )
+}
+
+export default ModalWrapper
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: isIos ? spacingY._15 : 20,
+        paddingBottom: isIos ? spacingY._20 : spacingY._10,
+    }
+})
