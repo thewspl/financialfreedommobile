6738751777350b8b7626060068d84ff25bfcb171
Baris Korkmaz <barskorkmaz6@gmail.com>
2025-04-30 18:17:18 +0300

    istatistikler için modül eklemesi yapıldı

--- utils/common.ts
+++ utils/common.ts
@@ -14,3 +14,53 @@ export const getLast7Days = () => {
     }
     return result.reverse();
 }
+
+export const getLast12Months = () => {
+    const monthsOfYear = [
+        "Oca",
+        "Şub",
+        "Mar",
+        "Nis",
+        "May",
+        "Haz",
+        "Tem",
+        "Auğ",
+        "Eyl",
+        "Eki",
+        "Kas",
+        "Ara",
+    ];
+    const result = [];
+
+    for (let i = 11; i >= 0; i--) {
+        const date = new Date();
+        date.setMonth(date.getMonth() - i);
+
+        const monthName = monthsOfYear[date.getMonth()];
+        const shortYear = date.getFullYear().toString().slice(-2);
+        const formattedMonthYear = `${monthName} ${shortYear}`;
+        const formattedDate = date.toISOString().split("T")[0];
+
+        result.push({
+            month: formattedMonthYear,
+            fullDate: formattedDate,
+            income: 0,
+            expense: 0,
+        });
+    }
+
+    return result.reverse();
+};
+
+export const getYearsRange = (startYear: number, endYear: number): any => {
+    const result = [];
+    for (let year = startYear; year <= endYear; year++) {
+        result.push({
+            year: year.toString(),
+            fullDate: `01-01-${year}`,
+            income: 0,
+            expense: 0,
+        });
+    }
+    return result.reverse();
+};
